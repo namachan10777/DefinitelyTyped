b8fe0b078df2c1ee7bef926efd7fdae24b366583
namachan10777 <namachan10777@gmail.com>
2018-06-26 17:29:43 +0900

    better-combine-reducers: fix indent

--- types/better-combine-reducers/better-combine-reducers-tests.ts
+++ types/better-combine-reducers/better-combine-reducers-tests.ts
@@ -2,17 +2,17 @@ import { Reducer, Action, combineReducers } from 'redux';
 import better from 'better-combine-reducers';
 
 interface TestStore {
-	a: number;
-	b: string;
+    a: number;
+    b: string;
 }
 
 let initialState = {
-	a: 0,
-	b: ""
+    a: 0,
+    b: ""
 }
 
 const AReducer: (state: number, action: Action) => number = (a, b) => a;
-
 const BReducer: (state: string, action: Action) => string = (a, b) => a;
+
 const reducer: (state: TestStore, action: Action) => TestStore =
-	better(combineReducers)({a: AReducer, b: BReducer}, initialState);
+    better(combineReducers)({a: AReducer, b: BReducer}, initialState);
--- types/better-combine-reducers/index.d.ts
+++ types/better-combine-reducers/index.d.ts
@@ -6,11 +6,11 @@ export type combineReducersA<S> = (reducers: Redux.ReducersMapObject<S, any>) =>
 export type combineReducersB<S, A extends Redux.Action = Redux.AnyAction> = (reducers: Redux.ReducersMapObject<S, A>) => Redux.Reducer<S, A>;
 
 export default function betterCombineReducers<S>
-	(original: combineReducersA<S>):
-	(reducers: Redux.ReducersMapObject<S, any>, initialState: PreloadedState<S>) =>
-	Redux.Reducer<S>;
+    (original: combineReducersA<S>):
+    (reducers: Redux.ReducersMapObject<S, any>, initialState: PreloadedState<S>) =>
+    Redux.Reducer<S>;
 
 export default function BetterCombineReducers<S, A extends Redux.Action = Redux.AnyAction>
-	(original: combineReducersB<S, A>):
-	(reducers: Redux.ReducersMapObject<S, A>, initialState: PreloadedState<S>) =>
-	Redux.Reducer<S, A>;
+    (original: combineReducersB<S, A>):
+    (reducers: Redux.ReducersMapObject<S, A>, initialState: PreloadedState<S>) =>
+    Redux.Reducer<S, A>;
