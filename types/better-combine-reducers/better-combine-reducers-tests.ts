71c9117c0534f49a5349062d9024b8513941288f
namachan10777 <namachan10777@gmail.com>
2018-06-26 16:13:18 +0900

    fix better-combine-reducers test

---
+++ types/better-combine-reducers/better-combine-reducers-tests.ts
@@ -0,0 +1,18 @@
+import { Reducer, Action, combineReducers } from 'redux';
+import better from 'better-combine-reducers';
+
+interface TestStore {
+	a: number;
+	b: string;
+}
+
+let initialState = {
+	a: 0,
+	b: ""
+}
+
+const AReducer: (state: number, action: Action) => number = (a, b) => a;
+
+const BReducer: (state: string, action: Action) => string = (a, b) => a;
+const reducer: (state: TestStore, action: Action) => TestStore =
+	better(combineReducers)({a: AReducer, b: BReducer}, initialState);
