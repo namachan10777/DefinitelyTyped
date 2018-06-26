d8a9fe529aaf4bc5e0d022f37b4c68a46888f257
namachan10777 <namachan10777@gmail.com>
2018-06-26 14:14:24 +0900

    define betterCombineReducers

---
+++ types/better-combine-reducers/index.d.ts
@@ -0,0 +1,16 @@
+import * as Redux from 'redux';
+
+export type PreloadedState<S> = Redux.DeepPartial<S>;
+
+export type combineReducersA<S> = (reducers: Redux.ReducersMapObject<S, any>) => Redux.Reducer<S>;
+export type combineReducersB<S, A extends Redux.Action = Redux.AnyAction> = (reducers: Redux.ReducersMapObject<S, A>) => Redux.Reducer<S, A>;
+
+export default function betterCombineReducers<S>
+	(original: combineReducersA<S>):
+	(reducers: Redux.ReducersMapObject<S, any>, initialState: PreloadedState<S>) =>
+	Redux.Reducer<S>;
+
+export default function BetterCombineReducers<S, A extends Redux.Action = Redux.AnyAction>
+	(original: combineReducersB<S, A>):
+	(reducers: Redux.ReducersMapObject<S, A>, initialState: PreloadedState<S>) =>
+	Redux.Reducer<S, A>;
